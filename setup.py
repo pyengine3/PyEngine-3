9dfbac3fdec33a8fa24ae456356c3c050df28f61
LavaPower <lavapower84@gmail.com>
2020-08-03 04:27:18 +0200

    Create Basic Repo

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import pyengine3
+
+setup(
+    name="pyengine3",
+    version=pyengine3.__version__,
+    packages=find_packages(),
+    author="LavaPower",
+    author_email="lavapower84@gmail.com",
+    description="A new version of PyEngine using Pyglet",
+    long_description=open("README.md").read(),
+    include_package_data=True,
+    url="http://github.com/pyengine-2D/PyEngine3",
+
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Intended Audience :: Developers",
+    ],
+    install_requires=['pyglet==1.5.7'],
+)
